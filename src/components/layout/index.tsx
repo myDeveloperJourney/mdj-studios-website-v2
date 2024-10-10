4e9c21dd5c2d7c9ba99dd900470c6dadf6e49f2f
Daniel Scott <daniel@mydeveloperjourney.com>
2024-10-10 17:45:22 -0500

    wip: installs speed insights

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,6 +3,7 @@ import Main from "./Main";
 import Footer from "./Footer";
 import Navbar from "../ui/Navbar";
 import React, { ReactNode } from 'react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 interface LayoutProps {
     children: ReactNode;
@@ -15,6 +16,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             <Hero />
             <Main>
                 {children}
+                <SpeedInsights />
             </Main>
             <Footer />
         </div>
