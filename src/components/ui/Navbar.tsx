dbcecf1c55be190188e281acbe2c689b7da27381
Daniel Scott <daniel@mydeveloperjourney.com>
2024-10-19 18:09:32 -0500

    wip: adds articles nav option

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -116,6 +116,13 @@ export default function Navbar() {
                                 >
                                     Contact Us
                                 </Link>
+                                <Link
+                                    href="/articles"
+                                    // onClick={(e) => handleSmoothScroll(e, "#contact")}
+                                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                                >
+                                    Articles
+                                </Link>
                             </div>
                         </div>
                     </div>
